1aae9275ef29df2e919db3434720c06467943589
githubeuse <julie.souvandy.pro@gmail.com>
2024-11-13 23:49:46 +0100

    mise en place de react via create app react

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
-
+  // process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  `mongodb+srv://juliesouvandypro:2VV1pdBp5jTFpYEP@cluster0.bxoyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true });
+    console.log("Database successfully connected");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
-}
+};
